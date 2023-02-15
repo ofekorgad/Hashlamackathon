7ab62c7c130dd895daacefd5d0462d8bfc763493
Ofek Orgad <Ofek@Ofek-Orgads-Mac.local>
2023-02-15 18:27:38 +0200

    Refactor

---
+++ enums/text_types.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    Essay = 1,
+    SocialMedia = 2,
+    Generic = 3
--- example/main.py
+++ example/main.py
@@ -1,5 +1,6 @@
 import base64
 import requests
+from enums.text_types import TextType
 
 
 API = "http://127.0.0.1:5000/run"
@@ -8,9 +9,9 @@ API = "http://127.0.0.1:5000/run"
 s = {
     "metadata": {
         "strict": 0,
-        "type": 1
+        "type": TextType.Generic.value
     },
-    "data": base64.encodebytes(b"This is my text to filter.").decode()
+    "data": base64.encodebytes(b"Please help. I no good.").decode()
 }
 
 
